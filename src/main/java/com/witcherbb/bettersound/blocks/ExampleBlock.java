8be9f09edf1937d63db8370e5f90f52412f30a77
WitcherBB <2839272344@qq.com>
2024-09-23 16:33:28 +0800

    新增setPlacedBy方法

--- src/main/java/com/witcherbb/bettersound/blocks/ExampleBlock.java
+++ src/main/java/com/witcherbb/bettersound/blocks/ExampleBlock.java
@@ -3,10 +3,13 @@ package com.witcherbb.bettersound.blocks;
 import com.witcherbb.bettersound.blocks.entity.ExampleBlockEntity;
 import com.witcherbb.bettersound.blocks.entity.utils.TickableBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Blocks;
@@ -46,6 +49,14 @@ public class ExampleBlock extends BaseEntityBlock {
 			}
 		}
 		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack) {
+		if (!pLevel.isClientSide) {
+			BlockPos pos = pPos.relative(Direction.NORTH);
+			pLevel.setBlock(pos, pState, ExampleBlock.UPDATE_ALL);
+		}
 	}
 
 	@Nullable
